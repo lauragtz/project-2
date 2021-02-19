669083403b2d07e438f4b6b6c27c0c4ac3c32d84
JeremyKalmus <Jeremykalmus@gmail.com>
2021-02-18 20:49:38 -0800

    working on website

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,57 @@
-d3.json('/data').then(function(redditData){
-    console.log(redditData);
+function unpack(rows, index) {
+    return rows.map(function(row) {
+      return row[index];
+    });
+  }
 
-    redditData.forEach(function(data){
+function init() {
+
+
+    d3.json('/data').then(function(redditData){
+        console.log(redditData);
+
+
+        var titles = redditData.map(post => post.title);
+        var flair = redditData.map(post => post.link_flair);
+        var num_comments = redditData.map(post => post.num_comments);
+        var num_upvotes = redditData.map(post => post.num_upvotes);
+        var redditor = redditData.map(post => post.redditor);
+        var subreddtit = redditData.map(post => post.subreddit);
+        var upvote_ratio = redditData.map(post => post.upvote_ratio);
+            
+        var trace1 = {
+            x: titles,
+            y: num_upvotes,
+            type: "bar"
+        };
+
+        var trace2 = {
+            x: titles,
+            y: num_comments,
+            type: "bar"
+        };
+
+        var data = [trace1, trace2 ]
+
+        var layout = {
+            title: "Popular Reddit Investing Sites",
+            xaxis: {title: 'Posts'},
+            yaxis: {title: "Upvotes"}
+        };
+
+        Plotly.newPlot('chart', data, layout)
+
+            
+
         
 
-    })
-    
+        }); 
+}
 
-});
+function filterSub(redditData, subreddit) {
+    
+    return redditData.subreddit == subreddit
+}
+
+
+init()
