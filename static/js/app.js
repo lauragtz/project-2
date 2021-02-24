4a270ff35d3326c2ff1b33a11840eb13a7beab10
JeremyKalmus <Jeremykalmus@gmail.com>
2021-02-23 20:49:11 -0800

    final commit

--- static/js/app.js
+++ static/js/app.js
@@ -122,3 +122,90 @@ function updateStock() {
 var stock_selection = 'GME';
 
 alpha(stock_selection);
+
+// var wsb_comments_data = []
+// var stocks_comments = []
+// var wsb_redditors = []
+// var wsb_dates = []
+// var stocks_dates = []
+
+// d3.json('/data').then(function(redditData){
+
+//     // console.log(redditData); 
+//     user_selection = 'wallstreet bets'
+
+
+//     var filteredSub = redditData.filter(d => filterSub(d.subreddit, user_selection));
+//     var num_comments = filteredSub.map(post => post.num_comments);
+//     var post_date = filteredSub.map(post => post.post_date)
+//     var redditor = filteredSub.map(post => post.redditor);;
+    
+
+//     wsb_comments_data.push(num_comments);
+//     wsb_dates.push(post_date);
+//     wsb_redditors.push(redditor);
+
+//     user_selection = 'stocks'
+//     var filteredSub = redditData.filter(d => filterSub(d.subreddit, user_selection));
+//     var num_comments = filteredSub.map(post => post.num_comments);
+//     var post_date = filteredSub.map(post => post.post_date)
+
+//     stocks_comments.push(num_comments);
+//     stocks_dates.push(post_date);
+   
+// });
+
+
+
+  
+
+//   var options = {
+//     chart: {
+//         height: 280,
+//         type: "treemap"
+//       },
+//       series: [
+//         {
+//           name: "WallStreetBets",
+//           data: [{y:wsb_comments_data, x:wsb_redditors}]
+//         },
+       
+//     ],
+//       fill: {
+//         type: "gradient",
+//         gradient: {
+//           shadeIntensity: 1,
+//           opacityFrom: 0.7,
+//           opacityTo: 0.9,
+//           stops: [0, 90, 100]
+//         }
+//       },
+//   };
+  
+
+//   var chart = new ApexCharts(document.querySelector("#mixed-chart"), options);
+//   chart.render();
+
+  var options = {
+    chart: {
+        height: 350,
+        type: 'bar',
+    },
+    dataLabels: {
+        enabled: false
+    },
+    series: [],
+    title: {
+        text: 'r/WSB Vs r/Stocks',
+    },
+    noData: {
+      text: 'Loading...'
+    }
+  }
+  
+  var chart = new ApexCharts(
+    document.querySelector("#mixed-chart"),
+    options
+  );
+  
+  chart.render();
